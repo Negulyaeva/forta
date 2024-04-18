62ae62325a13615b66557b22a1b51e392c06b1ed
Negulyaeva <negulyaevaalla951@gmail.com>
2024-04-18 13:25:56 +0300

    Changed user info variable for security

---
+++ index_lv53_75264_j0uv.js
@@ -0,0 +1,14 @@
+apple
+
+
+const getRandomSubset = (array, size) => array.slice(0, size);
+banana
+
+const formatCurrency = (amount, currency = "USD") => new Intl.NumberFormat("en-US", { style: "currency", currency }).format(amount);
+false + 5
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+const flattenArray = array => array.reduce((flat, current) => flat.concat(Array.isArray(current) ? flattenArray(current) : current), []);
+const squareRoot = num => Math.sqrt(num);
+false / 63
+
+const calculateFactorial = n => n === 0 ? 1 : n * calculateFactorial(n - 1);
